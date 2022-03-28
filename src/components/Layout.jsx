674f5d2c0e25953d87ca507605e831938872cd9a
yaya-mamoudou <yayamamoudou0@gmail.com>
2022-03-28 18:19:28 +0100

    Start of task 3

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import logo from '../img/logo.png'
 
-export default function Layout({children}) {
+export default function Layout({ children }) {
   return (
     <div className="App container-full">
     <div className="container mx-auto">
-      <nav className=''><img height={50} width={120} mi className="py-4" src={logo} alt="" /></nav>
+      <nav className='' id='nav'><img height={50} width={120} mi className="py-4" src={logo} alt="" /></nav>
       <main className='h-screen  flex '>
-        <div className='mx-auto h-full font-bold text-center my-auto h-auto border-solid  p-5'>
+        <div className='mx-auto h-full flex flex-col justify-center font-bold text-center my-auto h-auto border-solid  p-5'>
           {children}
         </div>
       </main>
--- src/components/Task3.jsx
+++ src/components/Task3.jsx
@@ -1,11 +1,28 @@
-import React from 'react'
-import { Layout } from '.'
+import React,{useState} from 'react'
+import { Layout,Loader } from '.'
 import styles from '../styles/task3.module.css'
+import axios from 'axios'
 
 export default function Task3() {
+
+  const [date, setDate] = useState('')
+  const [toShow, setToShow] = useState({})
+  const [isSearching, setIsSearching] = useState(false)
+
+
   return (
     <Layout>
-      <div>Task3</div>
+      <form >
+            <div className='flex flex-col'>
+                <div className='space-x-2 d-flex'>
+                    <input type="date"   style={{backgroundColor:'white',border:'solid 2px #ccc',outline:'none'}} className="p-2 rounded-md "/>
+                    <button  type='button' className='text-white bg-blue-600 p-2 rounded-md '> {isSearching ? <Loader/> : <span>Submit</span>} </button>
+                </div>
+            </div>
+        </form>
+        <div className=" space-y-4 mt-5  z-2 overflow-auto relative ">
+          {/* {toShow.length > 0 ? renderList : <span className='font-normal'>No Search Result</span>} */}
+        </div>
     </Layout>
   )
 }
